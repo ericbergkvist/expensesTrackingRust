fa5207b1879a09567ecc669ace76b700764be7ed
ericbergkvist <eric.bergkvist@epfl.ch>
2024-02-18 23:37:39 +0100

    Add serde json implementation to handle config

--- expenses_tracking/src/expense_tracker.rs
+++ expenses_tracking/src/expense_tracker.rs
@@ -1,13 +1,18 @@
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
 
+use serde::{Deserialize, Serialize};
+use std::fs::{File, OpenOptions};
+
 use crate::transaction::Transaction;
 
 /// A struct that deals with expense tracking
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ExpenseTracker {
     pub valid_categories: HashSet<String>,
     pub valid_subcategories: HashMap<String, HashSet<String>>,
+    #[serde(skip_serializing)]
+    #[serde(skip_deserializing)]
     pub transactions: Vec<Transaction>,
 }
 
@@ -60,6 +65,112 @@ impl ExpenseTracker {
         self.transactions.push(transaction);
         Ok(())
     }
+
+    // Load transactions from a CSV and generate an expense tracker
+    pub fn load_transactions_from_file(
+        &mut self,
+        file_path: &str,
+        generate_categories_and_sub: bool,
+        verbose: bool,
+    ) -> Result<(), Box<dyn Error>> {
+        // Open the CSV file
+        let file = match File::open(file_path) {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("Error while loading the CSV of transactions.");
+                return Err(e.into());
+            }
+        };
+
+        // Create a CSV reader
+        let mut rdr = csv::Reader::from_reader(file);
+
+        // Read the header record (first line)
+        if let Ok(header) = rdr.headers() {
+            // Convert the header entries into a Vec<String>
+            let header_list: Vec<String> = header.iter().map(|entry| entry.to_string()).collect();
+
+            // Print the resulting Vec<String>
+            println!("{:?}", header_list);
+        } else {
+            eprintln!("CSV file does not have a header row.");
+        }
+
+        let mut n_ignored_transactions: i32 = 0;
+
+        // Iterate over each record in the CSV file
+        for record in rdr.records() {
+            // Handle each CSV record
+            let csv_row = record?;
+
+            let transaction = Transaction::from_csv_row(csv_row)?;
+
+            if generate_categories_and_sub {
+                self.add_category(&transaction.category);
+
+                if let Some(transaction_subcategory) = &transaction.subcategory {
+                    // We can directly unwrap this function because we just made the category of the
+                    // transaction valid, meaning that this should not error
+                    self.add_subcategory(&transaction.category, transaction_subcategory)
+                        .unwrap();
+                }
+            }
+
+            match self.add_transaction(transaction) {
+                Ok(()) => (),
+                Err(e) => {
+                    if verbose {
+                        println!("{}", e);
+                    }
+                    n_ignored_transactions += 1;
+                }
+            };
+        }
+
+        if verbose {
+            println!(
+                "Number of valid transactions extracted from the CSV: {}",
+                self.transactions.len()
+            );
+            println!("Number of transactions ignored: {}", n_ignored_transactions);
+        }
+
+        Ok(())
+    }
+
+    // Load categories and sub-categories from a file
+    pub fn load_info_from_file(file_path: &str) -> Result<Self, Box<dyn Error>> {
+        let file = File::open(file_path)?;
+        let reader = std::io::BufReader::new(file);
+        let expense_tracker: ExpenseTracker = serde_json::from_reader(reader)?;
+        Ok(expense_tracker)
+    }
+
+    // Load categories and sub-categories from the transactions part of the expense tracker
+    pub fn load_info_from_transactions(&mut self) {
+        let cloned_transactions = self.transactions.clone();
+        for transaction in cloned_transactions {
+            self.add_category(&transaction.category);
+            if let Some(transaction_subcategory) = &transaction.subcategory {
+                // We can directly unwrap this function because we just made the category of the
+                // transaction valid, meaning that this should not error
+                self.add_subcategory(&transaction.category, transaction_subcategory)
+                    .unwrap();
+            }
+        }
+    }
+
+    // Save categories and sub-categories to a file
+    pub fn save_info_to_file(&self, file_path: &str) -> Result<(), Box<dyn Error>> {
+        let file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(file_path)?;
+        let writer = std::io::BufWriter::new(file);
+        serde_json::to_writer_pretty(writer, &self)?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -80,9 +191,6 @@ mod tests {
             }
         }
     }
-
-    // TODO: add tests for transaction parsing from CSV
-    // TODO: add helper functions to avoid duplicating code
 
     #[test]
     #[should_panic(expected = "Sub-category cannot be added because its category is invalid")]
--- expenses_tracking/src/main.rs
+++ expenses_tracking/src/main.rs
@@ -1,85 +1,22 @@
-use core::f32;
 use std::error::Error;
-use std::fs::File;
 
 mod expense_tracker;
 mod transaction;
 
 use expense_tracker::ExpenseTracker;
-use transaction::Transaction;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Specify the path to your CSV file
     //let file_path = "/home/ericbergkvist/personal/expensesTrackingRust/transactions.csv";
-    let file_path = "/Users/eric/Desktop/transactions.csv";
-
-    // Open the CSV file
-    let file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("Error while loading the CSV of transactions.");
-            return Err(e.into());
-        }
-    };
-
-    // Create a CSV reader
-    let mut rdr = csv::Reader::from_reader(file);
-
-    // Read the header record (first line)
-    if let Ok(header) = rdr.headers() {
-        // Convert the header entries into a Vec<String>
-        let header_list: Vec<String> = header.iter().map(|entry| entry.to_string()).collect();
-
-        // Print the resulting Vec<String>
-        println!("{:?}", header_list);
-    } else {
-        eprintln!("CSV file does not have a header row.");
-    }
-
-    let mut n_lines: i32 = 0;
-    let mut n_ignored_transactions: i32 = 0;
+    let transactions_file_path = "/Users/eric/Desktop/transactions.csv";
 
     let mut expense_tracker = ExpenseTracker::new();
-    expense_tracker.add_category("Nourriture");
-    expense_tracker.add_subcategory("Nourriture", "Courses")?;
-    expense_tracker.add_subcategory("Nourriture", "Restaurant")?;
+    expense_tracker.load_transactions_from_file(transactions_file_path, true, true)?;
 
-    // Iterate over each record in the CSV file
-    for record in rdr.records() {
-        // Handle each CSV record
-        let csv_row = record?;
+    // println!("{:?}", expense_tracker);
 
-        let transaction = Transaction::from_csv_row(csv_row)?;
-
-        match expense_tracker.add_transaction(transaction) {
-            Ok(()) => (),
-            Err(e) => {
-                println!("{}", e);
-                n_ignored_transactions += 1;
-            }
-        };
-
-        n_lines += 1;
-    }
-
-    let sum_transactions: f32 = expense_tracker
-        .transactions
-        .iter()
-        .map(|transaction| transaction.amount)
-        .sum();
-    println!("Sum of all transaction: {} CHF", sum_transactions);
-
-    println!(
-        "Number of lines in the CSV (excluding the header): {}",
-        n_lines
-    );
-    println!(
-        "Number of valid transactions extracted from the CSV: {}",
-        expense_tracker.transactions.len()
-    );
-    println!("Number of transactions ignored: {}", n_ignored_transactions);
-
-    println!("{:?}", expense_tracker);
+    let config_file_path = "config/expenseTrackerConfig.json";
+    expense_tracker.save_info_to_file(config_file_path)?;
 
     Ok(())
 }
