2340fcb4c8781b3a735ddce6571c59d5fb99e11f
ericbergkvist <eric.bergkvist@epfl.ch>
2024-04-20 12:47:23 +0200

    Add dummy GUI as a new module

---
+++ gui/main.rs
@@ -0,0 +1,54 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Expenses Tracking GUI",
+        options,
+        Box::new(|_cc| Box::<ExpensesTrackingGUI>::default()),
+    )
+}
+
+struct ExpensesTrackingGUI {
+    name: String,
+    age: u32,
+}
+
+impl Default for ExpensesTrackingGUI {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for ExpensesTrackingGUI {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let name_before_update = self.name.clone();
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Expenses Tracking GUI");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+
+        if self.name != name_before_update {
+            println!("Name change to {}", self.name);
+        }
+    }
+}
